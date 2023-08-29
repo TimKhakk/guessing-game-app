b754b55ce1366dff3200796edda96d389ccd15fd
Timur Khakkiev <khakkievtimur@yandex.ru>
2023-08-29 08:13:36 +0300

    update tw config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        colorBg0: 'rgb(202, 216, 228)',
+        colorBg1: 'hsl(209, 36%, 86%)',
+        colorBg2: 'hsl(224, 44%, 95%)',
+        colorTheme1: '#ff3e00',
+        colorTheme2: '#4075a6',
+        colorText: 'rgba(0, 0, 0, 0.7)',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
